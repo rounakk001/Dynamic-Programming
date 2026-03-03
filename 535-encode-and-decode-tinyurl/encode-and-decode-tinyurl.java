f630ff388b3d43b4362099a4fa17ee0dedc16019
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-03-03 17:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (63.24%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
@@ -0,0 +1,12 @@
+public class Codec {
+
+    
+    public String encode(String longUrl) {
+        return longUrl;   // koi shortning hogi bsdk
+    }
+
+    
+    public String decode(String shortUrl) {
+        return shortUrl;  // same cheez wapas bitchhhh
+    }
+}
