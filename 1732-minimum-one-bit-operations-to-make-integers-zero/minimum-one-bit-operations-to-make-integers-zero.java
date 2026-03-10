d80a7aa9f73d831fb9e03f565e363064455cb096
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-03-10 23:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (95.60%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minimumOneBitOperations(int n) {
-        int multiplier = 1;
-        int res = 0;
-        while (n > 0) {
-            res += n ^ (n - 1) * multiplier;
-            multiplier = -1 * multiplier;
-            n &= n - 1;
+        int m=1;
+        int res=0;
+        while(n>0) {
+            res+=n^(n-1)*m;
+            m=-1 * m;
+            n&=n-1;
         }
         return Math.abs(res);
     }
