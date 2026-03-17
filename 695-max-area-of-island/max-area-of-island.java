569b32604984e5ddd9d68bd28af502eb3d52e7b1
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-03-18 00:05:50 +0530

    Time: 3 ms (25.81%) | Memory: 47.1 MB (10.55%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int dfs(int row,int col,int area,int[][] grid,int[][] vis){
+
+        int n=grid.length;
+        int m=grid[0].length;
+
+        vis[row][col]=1;
+
+        area=1;
+
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+
+        for(int i=0;i<4;i++){
+
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                area+=dfs(nrow,ncol,0,grid,vis);
+            }
+        }
+            return area;
+        
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+
+
+        int vis[][]=new int[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                vis[i][j]=0;
+            }
+        }
+        int maxarea=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]==0)
+                maxarea=Math.max(maxarea,dfs(i,j,0,grid,vis));
+            }
+        }
+
+       return maxarea;
+    }
+}
