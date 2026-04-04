c7edc47c6459b785b9dadf50a53b363392100a41
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-04-04 21:58:12 +0530

    Time: 29 ms (54.44%) | Memory: 58.9 MB (27.22%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String decodeCiphertext(String s, int rows) {
+        if(rows==0)   //edge cases
+        return "";
+        if(rows==1)
+        return s;   //edge cases
+       int column=s.length()/rows;
+       char arr[][]=new char[rows][column];
+       int k=0;
+
+       for(int i=0;i<rows;i++){
+        for(int j=0;j<column;j++){
+            arr[i][j]=s.charAt(k);
+            k++;
+        }
+       } 
+
+       StringBuilder sb=new StringBuilder();
+
+       for(int t=0;t<column;t++){
+          int i=0;
+          int j=t;
+          while(i<rows && j<column){
+             sb.append(arr[i][j]);
+             i++;
+             j++;
+          }
+       }
+
+       return sb.toString().replaceAll("\\s+$","");
+    }
+}
