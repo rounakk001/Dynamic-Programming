3233636d04cd8326478ef2b040296e8e54309bff
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-04-29 15:48:26 +0530

    Time: 54 ms (53.19%) | Memory: 54.8 MB (11.06%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const response1=await promise1;
+    const response2= await promise2;
+
+    return response1+response2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
