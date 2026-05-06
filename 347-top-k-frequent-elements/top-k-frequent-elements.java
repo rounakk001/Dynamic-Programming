2d458f54611611133f1174d697e9dccdb772a0c9
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-05-06 15:00:19 +0530

    Time: 15 ms (55.04%) | Memory: 47.9 MB (19.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        // Step 1: Frequency map
+        //  Frequency map banayenge
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        // Step 2: Max Heap based on frequency
+        //  Max Heap based on frequency 
         PriorityQueue<Integer> pq = new PriorityQueue<>(
             (a, b) -> map.get(b) - map.get(a)
         );
 
         pq.addAll(map.keySet());
 
-        // Step 3: Extract top k
+       
         int[] res = new int[k];
         for (int i = 0; i < k; i++) {
             res[i] = pq.poll();
