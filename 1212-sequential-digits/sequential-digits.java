6a00eefe1b1c1bc4d28d4a0f72fe8ab4302b5a77
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-07-13 10:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (87.35%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int len = 2; len <= 9; len++) {
+            for (int start = 1; start <= 10 - len; start++) {
+                int num = 0;
+                int digit = start;
+
+                for (int i = 0; i < len; i++) {
+                    num = num * 10 + digit;
+                    digit++;
+                }
+
+                if (num >= low && num <= high) {
+                    res.add(num);
+                }
+            }
+        }
+
+        return res;
+    }
+}
