cdb38819d214162278a585abb305882d98456a62
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-08-05 01:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (59.93%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+      
+        return result(preorder,Integer.MAX_VALUE,new int[]{0});
+    }
+
+    public TreeNode result(int[] pre,int bound,int i[]){
+        if(i[0]==pre.length || pre[i[0]]>bound){
+            return  null;
+        }
+
+        TreeNode root=new TreeNode(pre[i[0]++]);
+        root.left=result(pre,root.val,i);
+        root.right=result(pre,bound,i);
+
+        return root;
+    }
+}
