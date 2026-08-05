faea462de1d534c95fff675f054234dc063e33f3
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-08-06 00:14:18 +0530

    Time: 18 ms (47.07%) | Memory: 50.1 MB (62.52%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -1,26 +1,8 @@
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
 public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
     
-    private Stack<TreeNode> stack;
     public BSTIterator(TreeNode root) {
-        stack = new Stack<>();
-        TreeNode cur = root;
-        while(cur != null){
-            stack.push(cur);
-            if(cur.left != null)
-                cur = cur.left;
-            else
-                break;
-        }
+        pushAll(root);
     }
 
     /** @return whether we have a next smallest number */
@@ -30,25 +12,12 @@ public class BSTIterator {
 
     /** @return the next smallest number */
     public int next() {
-        TreeNode node = stack.pop();
-        TreeNode cur = node;
-        // traversal right branch
-        if(cur.right != null){
-            cur = cur.right;
-            while(cur != null){
-                stack.push(cur);
-                if(cur.left != null)
-                    cur = cur.left;
-                else
-                    break;
-            }
-        }
-        return node.val;
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
     }
 }
-
-/**
- * Your BSTIterator will be called like this:
- * BSTIterator i = new BSTIterator(root);
- * while (i.hasNext()) v[f()] = i.next();
- */
